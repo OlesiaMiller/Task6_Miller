0f7adb7bc1416482725f17b7dbf8ff2e621b696a
OlesiaMiller <52347583+OlesiaMiller@users.noreply.github.com>
2019-09-01 02:29:44 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var contact = document.getElementById('contacts');
+var hider  = document.getElementById('hider');
+var n = 0;
+
+document.getElementById('hider').onclick = function() {
+    contact.style.height = "450px";
+    document.getElementById('hider').style.display = "none";
+
+    
+    console.log('hello')
+  }
