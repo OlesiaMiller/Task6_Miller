b05dfd98cb0026b78f14db06a48049137e8dc961
OlesiaMiller <52347583+OlesiaMiller@users.noreply.github.com>
2019-09-04 22:17:44 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 var contact = document.getElementById('contacts');
-var hider  = document.getElementById('hider');
-var n = 0;
+var show  = document.getElementById('shower');
+var hide  = document.getElementById('hider');
+var form  = document.getElementById('form');
+var main  = document.getElementById('main');
+var description = document.getElementById('description');
+
+
 
 document.getElementById('hider').onclick = function() {
-    contact.style.height = "450px";
-    document.getElementById('hider').style.display = "none";
+    contact.style.height = "0";
+    form.style.display = "none";
+    hider.style.display = "none";
+    description.style.display = "none";
+    contact.classList.remove('tool');
+  }
 
-    
-    console.log('hello')
+  document.getElementById('shower').onclick = function() {
+    contact.style.height = "450px";
+    form.style.display = "block";
+    hider.style.display = "block";
+    description.style.display = "block";
+    contact.classList.toggle('tool');
+  
+
   }
